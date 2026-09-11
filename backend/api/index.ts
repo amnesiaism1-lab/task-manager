7114e28be7aed057c86d0f751bf0a38ddb1090ba
quangtus <stu725105183@hnue.edu.vn>
2026-09-11 20:09:49 +0700

    chore: remove redundant backend/api/index.ts to use api/index.js cleanly

--- backend/api/index.ts
+++
@@ -1,8 +0,0 @@
-import handler from '../dist/main';
-
-export default handler;
-
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = handler;
-  (module.exports as any).default = handler;
-}
